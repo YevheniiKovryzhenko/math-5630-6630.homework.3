7a605adc6a7c536dc03e3fdde328fead82605f11
Yevhenii Kovryzhenko <yzk0058@auburn.edu>
2024-10-14 14:02:55 -0500

    solved all questions

--- hw03.m
+++ hw03.m
@@ -1,4 +1,4 @@
-% Author: Your Name / your_email
+% Author: Yevhenii Kovryzhenko / yzk0058@auburn.edu
 % Date: 2024-09-01
 % Assignment Name: hw03
 
@@ -14,10 +14,35 @@ classdef hw03
             % :param: eval: a vector of x values to evaluate the interpolating polynomial
             % :return: a vector, the evaluations of resulting interpolating polynomial at x values in eval
 
-            n = length(eval);
-            y = zeros(n, 1);
+            n_eval = length(eval);
+            
+            [n_poly, ~] = size(data);
+            x_interp = data(1:n_poly, 1);
+            y_interp = data(1:n_poly, 2);
+            y = zeros(n_eval, 1);
+            
+            for i_eval = 1:n_eval
+                x = eval(i_eval);
 
-            % Your code goes here
+                q = 1;
+                for i = 1:n_poly
+                    q = q*(x - x_interp(i));
+                end
+                
+                tmp = 0;
+                for j = 1:n_poly
+                    kj = 1;
+                    for s = 1:n_poly
+                        if s == j
+                            continue
+                        end
+                        kj = kj/(x_interp(j) - x_interp(s));
+                    end
+
+                    tmp = tmp + kj*y_interp(j) / (x - x_interp(j));
+                end
+                y(i_eval) = tmp*q;
+            end
         end
 
         function p2()
@@ -31,7 +56,10 @@ classdef hw03
             % Then, make a comment/explanation on the trend of the error for **equally spaced nodes** as n increases for each function.
 
             % Write your comments here.
-            %
+            % It is clear that as N grows, the interpolation error for
+            % equally spaced nodes grows and is much greater than
+            % interpolation that uses Chebyshev nodes. This is due to the
+            % Runge's phenomenon.
             %
             %
             %
@@ -41,29 +69,29 @@ classdef hw03
 
             % |n  |                        Function       | Error with equally spaced nodes | Error with Chebyshev nodes  |
             % |---|---------------------------------------|---------------------------------|-----------------------------|
-            % |  5|                @(x)1./(1+25*x.^2)     |                                 |                             |
-            % | 10|                @(x)1./(1+25*x.^2)     |                                 |                             |
-            % | 15|                @(x)1./(1+25*x.^2)     |                                 |                             |
-            % | 20|                @(x)1./(1+25*x.^2)     |                                 |                             |
-            % | 25|                @(x)1./(1+25*x.^2)     |                                 |                             |
-            % | 30|                @(x)1./(1+25*x.^2)     |                                 |                             |
-            % | 35|                @(x)1./(1+25*x.^2)     |                                 |                             |
-            % | 40|                @(x)1./(1+25*x.^2)     |                                 |                             |
-            % | 45|                @(x)1./(1+25*x.^2)     |                                 |                             |
-            % | 50|                @(x)1./(1+25*x.^2)     |                                 |                             |
-            % | 55|                @(x)1./(1+25*x.^2)     |                                 |                             |
+            % |  5|                @(x)1./(1+25*x.^2)     | 4.3267e-01                      |  5.5589e-01                 |
+            % | 10|                @(x)1./(1+25*x.^2)     | 1.9156e+00                      |  1.0915e-01                 |
+            % | 15|                @(x)1./(1+25*x.^2)     | 2.1069e+00                      |  8.3094e-02                 |
+            % | 20|                @(x)1./(1+25*x.^2)     | 5.9768e+01                      |  1.5333e-02                 |
+            % | 25|                @(x)1./(1+25*x.^2)     | 7.5764e+01                      |  1.1411e-02                 |
+            % | 30|                @(x)1./(1+25*x.^2)     | 2.3847e+03                      |  2.0613e-03                 |
+            % | 35|                @(x)1./(1+25*x.^2)     | 3.1708e+03                      |  1.5642e-03                 |
+            % | 40|                @(x)1./(1+25*x.^2)     | 1.0438e+05                      |  2.8935e-04                 |
+            % | 45|                @(x)1./(1+25*x.^2)     | 1.4243e+05                      |  2.1440e-04                 |
+            % | 50|                @(x)1./(1+25*x.^2)     | 4.8178e+06                      |  3.9629e-05                 |
+            % | 55|                @(x)1./(1+25*x.^2)     | 6.6475e+06                      |  2.9383e-05                 |
             % |---|---------------------------------------|---------------------------------|-----------------------------|
-            % |  5|                     @(x)sin(pi*x)     |                                 |                             |
-            % | 10|                     @(x)sin(pi*x)     |                                 |                             |
-            % | 15|                     @(x)sin(pi*x)     |                                 |                             |
-            % | 20|                     @(x)sin(pi*x)     |                                 |                             |
-            % | 25|                     @(x)sin(pi*x)     |                                 |                             |
-            % | 30|                     @(x)sin(pi*x)     |                                 |                             |
-            % | 35|                     @(x)sin(pi*x)     |                                 |                             |
-            % | 40|                     @(x)sin(pi*x)     |                                 |                             |
-            % | 45|                     @(x)sin(pi*x)     |                                 |                             |
-            % | 50|                     @(x)sin(pi*x)     |                                 |                             |
-            % | 55|                     @(x)sin(pi*x)     |                                 |                             |
+            % |  5|                     @(x)sin(pi*x)     | 2.6754e-02                      |  1.3193e-02                 |
+            % | 10|                     @(x)sin(pi*x)     | 5.1645e-05                      |  6.0348e-06                 |
+            % | 15|                     @(x)sin(pi*x)     | 9.2162e-10                      |  2.0995e-11                 |
+            % | 20|                     @(x)sin(pi*x)     | 3.0112e-13                      |  1.3323e-15                 |
+            % | 25|                     @(x)sin(pi*x)     | 1.2485e-11                      |  1.1102e-15                 |
+            % | 30|                     @(x)sin(pi*x)     | 1.6042e-10                      |  1.3323e-15                 |
+            % | 35|                     @(x)sin(pi*x)     | 5.8851e-09                      |  1.6653e-15                 |
+            % | 40|                     @(x)sin(pi*x)     | 1.2282e-07                      |  1.7764e-15                 |
+            % | 45|                     @(x)sin(pi*x)     | 7.5566e-06                      |  2.1094e-15                 |
+            % | 50|                     @(x)sin(pi*x)     | 1.7865e-04                      |  2.1094e-15                 |
+            % | 55|                     @(x)sin(pi*x)     | 5.0905e-03                      |  2.2204e-15                 |
             % |---|---------------------------------------|---------------------------------|-----------------------------|
 
             eval_pts = linspace(-1, 1, 1000)';
@@ -102,36 +130,38 @@ classdef hw03
 
             % Write your comment here.
             %
-            %
+            % Given the two example functions, it does look like extreme
+            % Chebyshev nodes are have interpolation errors on the same 
+            % order as the regular Chebyshev nodes, but slightly worse. 
             %
             %
             %
 
             % |n  |                        Function       | Error with extreme Chebyshev nodes  |
             % |---|---------------------------------------|-------------------------------------|
-            % |  5|                @(x)1./(1+25*x.^2)     |                                     |
-            % | 10|                @(x)1./(1+25*x.^2)     |                                     |
-            % | 15|                @(x)1./(1+25*x.^2)     |                                     |
-            % | 20|                @(x)1./(1+25*x.^2)     |                                     |
-            % | 25|                @(x)1./(1+25*x.^2)     |                                     |
-            % | 30|                @(x)1./(1+25*x.^2)     |                                     |
-            % | 35|                @(x)1./(1+25*x.^2)     |                                     |
-            % | 40|                @(x)1./(1+25*x.^2)     |                                     |
-            % | 45|                @(x)1./(1+25*x.^2)     |                                     |
-            % | 50|                @(x)1./(1+25*x.^2)     |                                     |
-            % | 55|                @(x)1./(1+25*x.^2)     |                                     |
+            % |  5|                @(x)1./(1+25*x.^2)     | 6.3862e-01                          |
+            % | 10|                @(x)1./(1+25*x.^2)     | 1.3219e-01                          |
+            % | 15|                @(x)1./(1+25*x.^2)     | 9.9308e-02                          |
+            % | 20|                @(x)1./(1+25*x.^2)     | 1.7738e-02                          |
+            % | 25|                @(x)1./(1+25*x.^2)     | 1.3649e-02                          |
+            % | 30|                @(x)1./(1+25*x.^2)     | 2.4252e-03                          |
+            % | 35|                @(x)1./(1+25*x.^2)     | 1.8710e-03                          |
+            % | 40|                @(x)1./(1+25*x.^2)     | 3.3987e-04                          |
+            % | 45|                @(x)1./(1+25*x.^2)     | 2.5645e-04                          |
+            % | 50|                @(x)1./(1+25*x.^2)     | 4.6187e-05                          |
+            % | 55|                @(x)1./(1+25*x.^2)     | 3.5147e-05                          |
             % |---|---------------------------------------|-------------------------------------|
-            % |  5|                     @(x)sin(pi*x)     |                                     |
-            % | 10|                     @(x)sin(pi*x)     |                                     |
-            % | 15|                     @(x)sin(pi*x)     |                                     |
-            % | 20|                     @(x)sin(pi*x)     |                                     |
-            % | 25|                     @(x)sin(pi*x)     |                                     |
-            % | 30|                     @(x)sin(pi*x)     |                                     |
-            % | 35|                     @(x)sin(pi*x)     |                                     |
-            % | 40|                     @(x)sin(pi*x)     |                                     |
-            % | 45|                     @(x)sin(pi*x)     |                                     |
-            % | 50|                     @(x)sin(pi*x)     |                                     |
-            % | 55|                     @(x)sin(pi*x)     |                                     |
+            % |  5|                     @(x)sin(pi*x)     | 1.3357e-02                          |
+            % | 10|                     @(x)sin(pi*x)     | 1.1730e-05                          |
+            % | 15|                     @(x)sin(pi*x)     | 2.1000e-11                          |
+            % | 20|                     @(x)sin(pi*x)     | 1.6653e-15                          |
+            % | 25|                     @(x)sin(pi*x)     | 1.5543e-15                          |
+            % | 30|                     @(x)sin(pi*x)     | 1.3323e-15                          |
+            % | 35|                     @(x)sin(pi*x)     | 1.3323e-15                          |
+            % | 40|                     @(x)sin(pi*x)     | 2.2204e-15                          |
+            % | 45|                     @(x)sin(pi*x)     | 1.8874e-15                          |
+            % | 50|                     @(x)sin(pi*x)     | 1.9984e-15                          |
+            % | 55|                     @(x)sin(pi*x)     | 1.8874e-15                          |
             % |---|---------------------------------------|-------------------------------------|
 
             eval_pts = linspace(-1, 1, 1000)';
--- hw_test.m
+++ hw_test.m
@@ -7,6 +7,10 @@ hw_assert(abs(hw03_worker.p1([-1, 1;1, 2;3, 7], 0) - 1)<1e-8);
 hw_assert(abs(hw03_worker.p1([-1, 1;1, 2;3, 7; 4, 2], 0) + 0.8)<1e-8);
 hw_assert(abs(hw03_worker.p1([-1, 1;1, 2;3, 7; 4, 2; 5, 9], 0) + 6.25)<1e-8);
 
+
+% hw03_worker.p2();
+% hw03_worker.p3();
+
 function hw_assert(X)
     if X; fprintf('\t PASS\n'); else; fprintf('\t FAIL\n'); end
 end
